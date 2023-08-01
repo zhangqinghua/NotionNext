60612d9841c7e7f2b0ada3906b436950a7cf0c09
zhangqinghua <the.patron.saint.of.science@gmail.com>
2023-08-01 14:21:11 +0800

    add cron

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -4,7 +4,7 @@ import { loadNotionPageDataAndCache } from '@/lib/notion/getNotionData'
 export default async function handler(req, res) {
   console.log('定时任务开始执行')
   await loadNotionPageDataAndCache({ pageId: BLOG.NOTION_PAGE_ID, from: 'cron job' })
-  console.log('定时任务开始成功')
+  console.log('定时任务执行成功')
 
   // 返回响应数据
   res.status(200).send('success')
